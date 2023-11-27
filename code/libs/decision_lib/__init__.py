d7576bc208f5c7d63071327256357f8110f3797d
baburini <ivan.baburin@inf.ethz.ch>
2023-11-27 16:41:11 +0100

    New solver method

--- code/libs/decision_lib/__init__.py
+++ code/libs/decision_lib/__init__.py
@@ -10,10 +10,12 @@ from decision_lib.threshhold_cluster_mean_classiifier import ThresholdMeanClassi
 from decision_lib.threshhold_cluster_mean_basian_classifier import ThresholdMeanBayesianClassifier
 from decision_lib.cluster_hierarchy_mean_classifier import ClusterHierarchyMeanClassifier
 from decision_lib.cluster_hierarchy_density_classifier import ClusterHierarchyDensityClassifier
+from decision_lib.cluster_relative_hierarchy_mean_classifier import ClusterRelativeHierarchyMeanClassifier
 
 __all__ = ["ThresholdClassifier",
            "ThresholdMeanClassifier",
            "ThresholdMeanBayesianClassifier",
            "ClusterHierarchyMeanClassifier",
-           "ClusterHierarchyDensityClassifier"
-           ]
+           "ClusterHierarchyDensityClassifier",
+           "ClusterRelativeHierarchyMeanClassifier"
+             ]
